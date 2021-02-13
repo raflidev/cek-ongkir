40363bedd28dcbaeb3e42e54b199552fb5a3a2cf
Rafli Ramadhan <rafliramdhn@gmail.com>
2021-02-13 22:17:12 +0700

    finishing route

--- index.js
+++ index.js
@@ -8,19 +8,50 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-import { getProvince, getProvinceById } from "./src/util/api"
+import { getProvince, getProvinceById, getCity, getCityById, getCost } from "./src/util/api"
 
+app.get('/', (req, res) => {
+   res.json({
+      message: "Welcome To Unofficial Rajaongkir Starter Api",
+      createdby: "raflidev"
+   })
+})
 
-app.get('/api/province', async (req, res) => {
+app.get('/province', async (req, res) => {
    const data = await getProvince();
    if (data.rajaongkir.status.code == 200) {
       res.json(data.rajaongkir.results)
    }
 })
-app.get('/api/province/:id', async (req, res) => {
+app.get('/province/:id', async (req, res) => {
    const data = await getProvinceById(req.params.id);
    if (data.rajaongkir.status.code == 200) {
       res.json(data.rajaongkir.results)
+   }
+})
+app.get('/city', async (req, res) => {
+   const data = await getCity();
+   if (data.rajaongkir.status.code == 200) {
+      res.json(data.rajaongkir.results)
+   }
+})
+
+app.get('/city/:id', async (req, res) => {
+   const data = await getCityById(req.params.id);
+   if (data.rajaongkir.status.code == 200) {
+      res.json(data.rajaongkir.results)
+   }
+})
+
+app.get('/cost/:from/:to/:weight/:courier', async (req, res) => {
+   const data = await getCost(req.params.from, req.params.to, req.params.weight, req.params.courier);
+   if (data.rajaongkir.status.code == 200) {
+      res.json(
+         {
+            destination: data.rajaongkir.destination_details,
+            detail: data.rajaongkir.origin_details,
+            result: data.rajaongkir.results
+         })
    }
 })
 
--- src/util/api.js
+++ src/util/api.js
@@ -18,4 +18,35 @@ async function getProvinceById(id) {
    return data
 }
 
-export { getProvince, getProvinceById }
+async function getCity() {
+   const { data } = await axios.get("https://api.rajaongkir.com/starter/city", {
+      params: {
+         key: process.env.KEY
+      }
+   })
+   return data
+}
+async function getCityById(province) {
+   const { data } = await axios.get("https://api.rajaongkir.com/starter/city", {
+      params: {
+         key: process.env.KEY,
+         province: province
+      }
+   })
+   return data
+}
+
+async function getCost(from, to, weight, courier) {
+   const { data } = await axios.post("https://api.rajaongkir.com/starter/cost", {
+
+      key: process.env.KEY,
+      origin: from,
+      destination: to,
+      weight: parseInt(weight),
+      courier: courier
+
+   })
+   return data
+}
+
+export { getProvince, getProvinceById, getCity, getCityById, getCost }
